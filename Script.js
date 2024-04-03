7070b339d9f1deb439eb8e7e2c16bd0fe3d2abcb
Wi1on <165930016+Wi1on@users.noreply.github.com>
2024-04-03 23:04:57 +0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,41 @@
+function next1() {
+  const hasilImage = document.querySelector(".scroll1");
+  const direction = 1.1;
+  const scrollAmount = hasilImage.clientWidth * direction;
+  hasilImage.scrollBy({ left: scrollAmount, behavior: "smooth"});
+}
+
+function prev1() {
+  const hasilImage = document.querySelector(".scroll1");
+  const direction = -1.1;
+  const scrollAmount = hasilImage.clientWidth * direction;
+  hasilImage.scrollBy({ left: scrollAmount, behavior: "smooth"});
+}
+
+function next2() {
+  const hasilImage = document.querySelector(".scroll2");
+  const direction = 1.1;
+  const scrollAmount = hasilImage.clientWidth * direction;
+  hasilImage.scrollBy({ left: scrollAmount, behavior: "smooth"});
+}
+
+function prev2() {
+  const hasilImage = document.querySelector(".scroll2");
+  const direction = -1.1;
+  const scrollAmount = hasilImage.clientWidth * direction;
+  hasilImage.scrollBy({ left: scrollAmount, behavior: "smooth"});
+}
+
+function next3() {
+  const hasilImage = document.querySelector(".scroll3");
+  const direction = 1.1;
+  const scrollAmount = hasilImage.clientWidth * direction;
+  hasilImage.scrollBy({ left: scrollAmount, behavior: "smooth"});
+}
+
+function prev3() {
+  const hasilImage = document.querySelector(".scroll3");
+  const direction = -1.1;
+  const scrollAmount = hasilImage.clientWidth * direction;
+  hasilImage.scrollBy({ left: scrollAmount, behavior: "smooth"});
+}
